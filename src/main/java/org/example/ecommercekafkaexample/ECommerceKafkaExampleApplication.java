079cceeaac0d8a81e30154db18b7098049e6e850
Piotr Zbigniew Hic <piotrhic@gmail.com>
2026-02-05 12:56:38 +0100

    Init: create Spring Boot project. Closes #1

---
+++ src/main/java/org/example/ecommercekafkaexample/ECommerceKafkaExampleApplication.java
@@ -0,0 +1,13 @@
+package org.example.ecommercekafkaexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ECommerceKafkaExampleApplication {
+
+    public static void main ( String[] args ) {
+        SpringApplication.run ( ECommerceKafkaExampleApplication.class, args );
+    }
+
+}
